ca704746ed5613a64782adc93d96a5e6cdc066ba
Diqosh <d_tleuzhanuly@kbtu.kz>
2021-12-27 22:59:31 +0600

    learn slick

--- JS/testing_slider/js/myjs.js
+++ JS/testing_slider/js/myjs.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
         arrows: true,           // false удалить стрелки
         dots: true,             // false удалить точки перелистования
         adaptiveHeight: true,   // меняется высота трэка
-        slidesToShow: 1,        //сколько элемнтов в трэка за рах
+        slidesToShow: 3,        //сколько элемнтов в трэка за раз
         slidesToScroll: 1,      //сколько картинок меняется при челчке
         speed: 1000,            // скорость перелистования
         easing: 'linear',
@@ -17,15 +17,38 @@ $(document).ready(function () {
         draggable: true,          //swipe in comp
         swipe: true,              //swipe in phone
         touchThreshold: 10,    //сколько части экрана нужно пролистать что листатась
-        touchMove: false,       //не двигается при свайпе но свайпается
+        touchMove: true,       //не двигается при свайпе но свайпается
         waitForAnimate: true,  //ждать некоторые время после клика перелистования,
-        centerMode: false,      // первый в центре
+        centerMode: true,      // первый в центре
         variableWidth: false,     //на сколько возможно пытается вместить слайды в трэк с права не помещающиеся вырезает
         rows: 1,                     //сколько ряда
         slidesPerRow: 1,            //сколько column in one row
         vertical: false,               //стоит указать slider_item указать height и в родителе block
         verticalSwiping: false,         //двигается вверх вних при touch
         fade: false,                     //не двигает при touch постепенно исчезает и появляется
+        asNavFor: ".slider_big",         //связываем другой слайд с текущей
+        responsive: [
+            {
+                breakpoint: 1000,
+                settings: {
+                    slidesToShow: 2,
 
+                }
+            }, {
+                breakpoint: 800,
+                settings: {
+                    slidesToShow: 1,
+
+                }
+            },
+
+        ],
+        // appendArrows: $('.content'), // перемещает стрелки
+        // appendDots: $('.content'),// перемещает точки
+    })
+    $('.slider_big').slick({
+        arrows: false,
+        fade: true,
+        asNavFor: ".slider"
     })
 })
--- main.py
+++ main.py
@@ -1,15 +1,9 @@
-if __name__ == '__main__':
-    age = int(input('Enter age: '))
-    sex = input('Enter sex:\n M = Male F = Female\n ')
-    marital_st = input('Are u married?\n Y = yes N = No\n')
 
-    if sex == "F":
-        print(' she will work only in urban areas')
-        exit(0)
-    if sex == "M" and 20 <= age < 40:
-        print('he may work in anywhere')
-        exit(0)
-    if sex == "M" and 40 <= age < 60:
-        print('he will work in urban areas only.')
-        exit(0)
-    print('ERROR')
+
+def solution():
+    return True
+
+
+
+if __name__ == '__main__':
+    solution()
